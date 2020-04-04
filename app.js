f944c21b851b0b002967dbb1f490e9468eba938b
root <root@ip-172-16-0-223.eu-west-1.compute.internal>
2020-04-04 09:28:45 +0000

    testing a ssl cert

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ app.get('', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(80, () => console.log('Server running on port 80!'))
+
+/*const fs = require('fs')
+const https = require('https')
+https.createServer({
+  key: fs.readFileSync('/etc/ssl/certs/ssl/server.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/ssl/server.cert')
+}, app).listen(443, () => {
+  console.log('Listening...')
+})*/
