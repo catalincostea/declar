0be3587f332fed0b5acd2a4110be7d9768919f66
catalincostea@waterford.org <catalincostea@waterford.org>
2020-04-17 08:42:58 +0300

    morgan

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+
+
+var fs = require('fs')
+var morgan = require('morgan')
+//var path = require('path')
+
+//var app = express()
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
+
+
+
+
 app.use(express.static('.'))
 app.get('', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
